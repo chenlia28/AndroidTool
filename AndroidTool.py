3e2f4d22727640b0a7ef11cad18d4dc47a40ed06
chenlia28 <chenlia28@163.com>
2021-06-19 20:58:11 +0800

    Update AndroidTool.py
    support iOS screenshot and install ipa file.

--- AndroidTool.py
+++ AndroidTool.py
@@ -4,6 +4,7 @@
 '''
 @author: Liang.Chen
 @version: 2016-12-12
+new
 '''
 
 from Tkinter import *
@@ -11,41 +12,93 @@ import os
 # import tkMessageBox
 import datetime
 
-def  callback():
-    var.set('Thanks ! ')
+# def callback():
+#     var.set('Thanks ! ')
 
-def  Capture():
+def function_capture_android():
+    time = datetime.datetime.now()
+    nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    # print(nowTime)
+    # print(time)
+
     os.system('adb shell /system/bin/screencap -p /sdcard/screenshot.png')
-    os.system('adb pull /sdcard/screenshot.png')
-    var.set('Result: %s' % datetime.date.today())
+    os.system('adb pull /sdcard/screenshot.png  ./%s.png' % nowTime)
+    # var.set('Result: Finished at %s' % datetime.datetime.today())
 
-def  Install():
+    #打印文件名
+    var.set('Result: Success \n \n'
+        'Filename: %s.png' % nowTime)
+
+def function_install_android():
     os.system('adb install -r *.apk')
+    var.set('Result: Finished at %s' % datetime.datetime.today())
 
-def  Download_Dingtone_log():
+
+def function_capture_iOS():
+    time = datetime.datetime.now()
+    nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    current_dir = os.getcwd()
+    # print(current_dir)
+    # print(nowTime)
+    # print(time)
+    # os.system('tidevice /sdcard/screenshot.png  ./%s.png' % nowTime)
+    os.system('tidevice screenshot')
+    # sleep(2)
+    # var.set('Result: Finished at %s' % datetime.datetime.today())
+    os.system('mv screenshot.jpg  %s.png' % nowTime)
+
+    #打印文件名
+    file_name = current_dir + '/' + '%s.png' % nowTime
+    # print(file_name)
+    var.set('Result: ==== Success ==== \n \n'
+        'Save to: \n %s' % file_name)
+
+def function_install_iOS():
+    os.system('tidevice install *.ipa')
+    var.set('Result: Finished at %s' % datetime.datetime.today())
+
+
+
+def function_download_dingtone_log():
     os.system('adb pull /sdcard/Dingtone/log ./log')
 
-def  Download_TalkU_log():
+def function_download_talkU_log():
     os.system('adb pull /sdcard/Talkyou/log ./log')
-
-def  Download_DingtoneDebug_log():
-    os.system('adb pull /sdcard/DingtoneDebug/log ./log')
-
-def  Download_TalkUDebug_log():
-    os.system('adb pull /sdcard/TalkyouDebug/log ./log')
 
 # 创建下拉列表的group
 # li = ['* Capture Screenshot', '* Install APK', '* Log TalkU', '* Log TalkUDebug','* Log Dingtone','* Log DingtoneDebug']
 
 win = Tk()     # 初始旷的声明
-win.title('Android Tool Beta v0.9.0')
+win.title('Android Tool Beta v0.9.2，2016')
 content = win.command
 
 today = datetime.date.today()
 # print (today)
 
 frame1 = Frame(win)   # 在初始框里面 声明两个模块 .
-frame2 = Frame(win)
+frame2 = Frame(
+            master=win,  # 父容器
+            # bg='blue',  # 背景颜色
+            # relief='groove',  # 边框的3D样式 flat、sunken、raised、groove、ridge、solid。
+            # bd=60,  # 边框的大小
+            height=100,  # 高度
+            width=100,  # 宽度
+            padx=10,  # 内间距，字体与边框的X距离
+            pady=10,  # 内间距，字体与边框的Y距离
+            cursor='arrow',  # 鼠标移动时样式 arrow, circle, cross, plus...
+            )
+frame3 = Frame(
+            master=win,  # 父容器
+            # bg='yellow',  # 背景颜色
+            # relief='groove',  # 边框的3D样式 flat、sunken、raised、groove、ridge、solid。
+            # bd=30,  # 边框的大小
+            height=100,  # 高度
+            width=100,  # 宽度
+            padx=10,  # 内间距，字体与边框的X距离
+            pady=10,  # 内间距，字体与边框的Y距离
+            cursor='arrow',  # 鼠标移动时样式 arrow, circle, cross, plus...
+            )
+# frame4 = Frame(win)
 
 # 创建一个文本Label对象
 var = StringVar()           #声明可变 变量
@@ -67,26 +120,32 @@ OM.pack()
 # okButton = Button(frame2, text="OK", command=callback)
 # okButton.pack(side=LEFT)
 
-CaptureButton = Button(frame2, text="Capture", command=Capture)
-CaptureButton.pack(side=LEFT)
+CaptureButton_Android = Button(frame2, text="Android Screenshot", command=function_capture_android)
+CaptureButton_Android.pack(side=LEFT)
 
-InstallButton = Button(frame2, text="Install APK", command=Install)
-InstallButton.pack(side=LEFT)
+# InstallButton_Android = Button(frame2, text="Install apk（disable）", command=function_install_android)
+# InstallButton_Android.pack(side=LEFT)
 
-DownloadDingtonelogButton = Button(frame2, text="Dingtone Log", command=Download_Dingtone_log)
-DownloadDingtonelogButton.pack(side=LEFT)
 
-DownloadDingtonelogButton = Button(frame2, text="DingtoneDebug Log", command=Download_DingtoneDebug_log)
-DownloadDingtonelogButton.pack(side=LEFT)
+CaptureButton_iOS = Button(frame3, text="iOS Screenshot", command=function_capture_iOS)
+CaptureButton_iOS.pack(side=LEFT)
 
-DownloadTalkUlogButton = Button(frame2, text="TalkU Log", command=Download_TalkU_log)
-DownloadTalkUlogButton.pack(side=LEFT)
+# InstallButton_iOS = Button(frame3, text="Install ipa（disable）", command=function_install_iOS)
+# InstallButton_iOS.pack(side=LEFT)
 
-DownloadTalkUlogButton = Button(frame2, text="TalkUDebug Log", command=Download_TalkUDebug_log)
-DownloadTalkUlogButton.pack(side=LEFT)
 
-frame1.pack(padx=100, pady=50)
-frame2.pack(padx=60, pady=40)
+
+# # 禁用
+# DownloadDingtonelogButton = Button(frame2, text="Dingtone Log", command=function_download_dingtone_log)
+# DownloadDingtonelogButton.pack(side=LEFT)
+#
+# # 禁用
+# DownloadTalkUlogButton = Button(frame2, text="TalkU Log", command=function_download_talkU_log)
+# DownloadTalkUlogButton.pack(side=LEFT)
+
+frame1.pack(padx=100, pady=60)
+frame2.pack(padx=20, pady=20)
+frame3.pack(padx=20, pady=20)
 
 win.resizable(0,0)  # 固定窗口的大小
 win.mainloop()
